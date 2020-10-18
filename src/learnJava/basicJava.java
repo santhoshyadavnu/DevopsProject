195f16f6e6b7f0fc459343ceb824891177b10804
santhoshyadavnu <santhoshyadavnu@gmail.com>
2020-10-18 17:49:21 +0530

    test build

--- src/learnJava/basicJava.java
+++ src/learnJava/basicJava.java
@@ -5,6 +5,7 @@ public class basicJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Project on dockerization of Jenkins pipeline");
+		System.out.print("Making changes to trigger the build - 1");
 	}
 
 }
